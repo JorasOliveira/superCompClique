e0e62f90cbbcf9ae5751b114024198a4c89ec3bd
JorasOliveira <joras.custodio@gmail.com>
2023-11-07 10:50:14 -0300

    atempt at a exhaustive solution

--- graph.h
+++ graph.h
@@ -1,16 +1,24 @@
 #ifndef GRAPH_H    // To make sure you don't declare the function more than once by including the header multiple times.
 #define GRAPH_H
 #include <vector>
+#include <algorithm>
+#include <set>
 #include <iostream>
 #include <fstream>
 #include <omp.h>
 // #include <mpi.h>
+using namespace std;
 
+
+#include <string>
+#include <vector>
 
 const std::string FILE_NAME = "graph.txt";
 int NUM_VERTICES = 1000;
 
-std::vector<std::vector<int>> readGraph(const std::string& FILE_NAME, int& numVertices);
+vector<vector<int>> readGraph(const string& FILE_NAME, int& NUM_VERTICES);
+
+vector<int> FindMaximumClique(vector<vector<int>> graph, int NUM_VERTICES);
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "graph.h"
 int main() {
 
-    std::vector<std::vector<int>> graph = readGraph(FILE_NAME, NUM_VERTICES);
+    vector<vector<int>> graph = readGraph(FILE_NAME, NUM_VERTICES);
 
-    
+    vector<int> maximumClique = FindMaximumClique(graph, NUM_VERTICES);
+
     return 0;
 }
